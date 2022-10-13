bc3c0299b93597be492592c8922658953d1b0ac7
MAXIME LEDRU <maxime.ledru4@orange.fr>
2022-10-13 10:10:20 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int heureJournee = DateTime.Now.Hour;
+        int minutesJournee = DateTime.Now.Minute;
+
+        if ((heureJournee >= 0 && minutesJournee >= 0) && (heureJournee <= 12 && minutesJournee >= 0))
+        {
+            Console.WriteLine("Bonjour");
+
+        }
+        else
+        {
+            Console.WriteLine("Bonsoir");
+        }
+    }
+}
